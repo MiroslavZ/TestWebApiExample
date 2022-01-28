73c4e154824084e3e063ea5bef382f4738c419be
MiroslavZ <zenkov-miroslav@mail.ru>
2022-01-28 21:24:49 +0500

    Implemented filtering, searching and sorting for employees

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 using Shared.RequestFeatures;
 
 namespace Repository
@@ -15,14 +16,12 @@ namespace Repository
 		public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
 		{
 			var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-			   .OrderBy(e => e.Name)
-			   .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
-			   .Take(employeeParameters.PageSize)
-			   .ToListAsync();
+					.FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)	
+					.Search(employeeParameters.SearchTerm)
+					.Sort(employeeParameters.OrderBy)
+					.ToListAsync();
 
-			var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
-
-			return new PagedList<Employee>(employees, count,employeeParameters.PageNumber, employeeParameters.PageSize);
+			return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
 		}
 
 		public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
